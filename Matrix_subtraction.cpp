471664bbff7fcc3e9afa13d143504cceb9b9e093
sbindal057 <shwetabindal2001@gmail.com>
2020-10-22 17:12:12 +0530

    Create Matrix_subtraction.cpp

---
+++ Matrix_subtraction.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class matrix
+{
+public:
+float **a;
+int r,c;
+matrix()
+{
+	r=0;c=0;
+	a = NULL;
+}
+matrix(int rr,int cc)
+{
+	r = rr; c = cc;
+	a = new float*[r+1];
+	for(int i=0;i<r;i++)a[i] = new float[c+1];
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)a[i][j] =0;
+
+	}
+
+}
+
+
+void get()
+{
+
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)cin>>a[i][j];
+	}
+}
+
+void add(matrix&b)
+{
+	if(b.r!=r || b.c!=c)cout<<"addition is not possible\n";
+	else
+	{
+		cout<<"addition\n";
+		for (int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)cout<<a[i][j] + b.a[i][j]<<" ";
+			cout<<"\n";
+		}
+	}
+}
+
+void subtract(matrix&b)
+{
+	if(b.r!=r || b.c!=c)cout<<"subtraction is not possible\n";
+	else
+	{
+		cout<<"subtraction\n";
+		for (int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)cout<<a[i][j] - b.a[i][j]<<" ";
+			cout<<"\n";
+		}
+	}
+}
+};
